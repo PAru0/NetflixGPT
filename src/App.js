bc74f97378fa6f3b656b4212e72b4829bcf3b8df
PAru0 <praveenkhankriyal1@gmail.com>
2024-07-20 13:43:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-8xl font-bold text-green-800 ">SIuu. I am react!hiiiiii!..
+    </div>
+  );
+}
+
+export default App;
